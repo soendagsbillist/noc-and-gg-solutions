26b7f264614e7b42f53b75243dfbf1b814f2a3c1
soendagsbillist <vladislav.tyutin@me.com>
2020-05-31 19:27:19 +0200

    Add createWind

--- forces/projectLeaves/sketch.js
+++ forces/projectLeaves/sketch.js
@@ -1,11 +1,11 @@
-let windowWidth = 800;
-let windowHeight = 300;
+let windowWidth = 1600;
+let windowHeight = 800;
 let font;
 let leaves = [];
-
-function fontRead(){
-    fontReady = true;
-}
+let mouseEvent = false;
+let w;
+let radius, angle;
+let flowfields = [];
 
 function preload() {
   font = loadFont('./AvenirNextLTPro-Demi.otf');
@@ -13,16 +13,22 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  frameRate(30);
+  w = createVector(0, 0);
+  angle = 0;
 
   //textFont('Arial');
 
   let points = font.textToPoints('effectvoll', 100, 200, 150);
+  pt = new Particle(0, 200);
 
   points.forEach(pt => {
     let leaf = new Particle(pt.x, pt.y);
     leaves.push(leaf);
+    let windForce = createWind(w);
+    let force = windForce.next().value;
+    flowfields.push(force);
   });
-
 }
 
 function draw() {
@@ -31,20 +37,49 @@ function draw() {
   leaves.forEach(leaf => {
     leaf.render();
     leaf.update();
-    leaf.steer();
   });
-  mousePressed();
-  mouseReleased();
+  
+  if(mouseEvent) {
+    const windP = createWind(w);
+    let forceP = windP.next().value;
+
+    leaves.forEach(leaf => {
+      w.add(0, random(2));
+      leaf.applyForce(forceP.add(w));
+    });
+  } else {
+    pt.steer();
+     leaves.forEach(leaf => {
+       leaf.steer();
+     });
+  }
 }
 
 function mousePressed(event) {
   if (event !== undefined) {
-    console.log(event.type);
+    mouseEvent = true;
   }
 }
 
 function mouseReleased(event) {
   if (event !== undefined) {
-    console.log(event.type);
+    mouseEvent = false;
+  }
+}
+
+// Wind -> Target
+// takes wind vector, modifies it and returns
+// target direction
+function *createWind(wind_) {
+  let inc = 0.1;
+  let target = createVector(1600, 200);
+
+  while(true) {
+    wind_.y = map(sin(angle), -1, 1, -800, 800);
+    angle += inc;
+    wind_.x = 700;
+
+    target.normalize();
+    yield target;
   }
 }
