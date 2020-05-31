0ccf970a4cf3c9d08cb1e1748e04bcc6fe4a5381
soendagsbillist <vladislav.tyutin@me.com>
2020-05-31 22:41:18 +0200

    Change render to consume an image and add repel force function

--- forces/projectLeaves/particle.js
+++ forces/projectLeaves/particle.js
@@ -1,11 +1,13 @@
 Particle = function(x, y) {
-  this.location = createVector(random(width), random(height));
-  //this.velocity = createVector(0, 0);
-  this.velocity = p5.Vector.random2D();
+  this.location = createVector(295, 150);
+  this.velocity = createVector(0, 0); 
   this.acceleration = createVector(0, 0);
   this.target = createVector(x, y);
-  this.maxSpeed = 5;
-  this.maxForce = 0.3;
+  this.maxSpeed = 20;
+  this.maxForce = 0.5;
+  this.img = loadImage('./leaf.png');
+  this.G = 5;
+  this.mass = 1;
 }
 
 Particle.prototype.update = function() {
@@ -43,12 +45,22 @@ Particle.prototype.seek = function(target) {
 }
 
 Particle.prototype.applyForce = function(force) {
+  force.limit(0.5);
   this.acceleration.add(force);
+  this.velocity.limit(10);
 }
 
 Particle.prototype.render = function() {
   stroke(255);
-  //strokeWeight(7);
-  //point(thix.location.x, this.location.y);
-  ellipse(this.location.x, this.location.y, 5, 5);
+  image(this.img, this.location.x, this.location.y);
+}
+
+Particle.prototype.repel = function(p) {
+  let force = p5.Vector.sub(this.location, p.location);
+  let distance = force.mag();
+
+  let strength = (this.G * this.mass * this.mass) / (distance * distance);
+  force.normalize();
+  force.mult(strength);
+  return force;
 }
